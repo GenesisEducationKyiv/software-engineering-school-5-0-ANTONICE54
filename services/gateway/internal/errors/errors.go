19cf74c5c3613cc408f7f1f87cbfdf3a6859deea
Antonice <oliynik262004@gmail.com>
2025-07-27 12:29:43 +0300

    Delete old error mappers

--- services/gateway/internal/errors/errors.go
+++ services/gateway/internal/errors/errors.go
@@ -8,24 +8,6 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func HTTPFromGRPC(err error, logger logger.Logger) (int, map[string]any) {
-	st, ok := status.FromError(err)
-	if !ok {
-		return http.StatusInternalServerError, map[string]any{"error": "unexpected error"}
-	}
-	switch st.Code() {
-	case codes.AlreadyExists:
-		return http.StatusConflict, map[string]any{"error": st.Message()}
-	case codes.InvalidArgument:
-		return http.StatusBadRequest, map[string]any{"error": st.Message()}
-	case codes.NotFound:
-		return http.StatusNotFound, map[string]any{"error": st.Message()}
-	default:
-		logger.Warnf("Unexpected gRPC error: %s", err.Error())
-		return http.StatusInternalServerError, map[string]any{"error": "internal server error"}
-	}
-}
-
 type (
 	HTTPReponse struct {
 		StatusCode int

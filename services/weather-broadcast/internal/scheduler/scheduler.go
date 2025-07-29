53a13b06e77ac6f269ee6b87fdad4e3fc503a8f0
Antonice <oliynik262004@gmail.com>
2025-07-29 22:50:56 +0300

    Change daily broadcat time

--- services/weather-broadcast/internal/scheduler/scheduler.go
+++ services/weather-broadcast/internal/scheduler/scheduler.go
@@ -9,8 +9,8 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-const DAILY = "00 12 * * * " //every day at 12 am
-const HOURLY = "0 * * * * "  //every hour at 0 minute
+const DAILY = "0 12 * * * " //every day at 12 am
+const HOURLY = "0 * * * * " //every hour at 0 minute
 
 type (
 	WeatherBroadcastService interface {
